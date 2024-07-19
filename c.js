3b2197c197505962b62344117429265db3483bf4
Abel Koech <koechabel161@gmail.com>
2024-07-19 13:07:43 +0300

    changes

---
+++ c.js
@@ -0,0 +1,20 @@
+/*Technologies used
+I use HTML to structure the content of webpage
+
+CSS to style HTML elements to enhance visual presentation
+
+JavaScript to add functionality and interactivity to webpage,
+also to fetch data  from MEALDB API to dynamically populate webpage with recipe.
+
+I used MEALDB API - to acces database of recipes.
+
+
+
+
+
+
+
+
+
+
+*/
